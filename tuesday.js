5785fa76cb048472c7999f6dbddae9bec4589d3d
HK Kahng <hk@router-rooter.com>
2016-11-10 10:34:11 -0800

    refactor into separate JS file

--- tuesday.js
+++ tuesday.js
@@ -12,3 +12,29 @@ return names.indexOf(name);
 
 
 console.log(weekDay.name(weekDay.number("Saturday"))); // Saturday
+
+(function(exports) {
+var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+exports.name = function(number) {
+return names[number];
+};
+exports.number = function(name) {
+return names.indexOf(name);
+};
+})(this.weekDay = {});
+
+$("#outputAreaWeekDay").text(weekDay.name(weekDay.number("Saturday")));
+
+(function(exports) {
+var names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+exports.name = function(number) {
+return names[number];
+};
+exports.number = function(name) {
+return names.indexOf(name);
+};
+})(this.month = {});
+
+$("#outputAreaMonth").text(month.name(month.number("November")));
