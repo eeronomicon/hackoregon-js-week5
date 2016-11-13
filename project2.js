f4c8addee7c98429cb8ea33070e16dbe02af06a5
HK Kahng <hk@router-rooter.com>
2016-11-12 18:26:01 -0800

    add project #2

---
+++ project2.js
@@ -0,0 +1,148 @@
+"use strict";
+
+(function(exports) {
+
+  exports.projectName = "classToolBox";
+  exports.createdFor = "Created for Hack Oregon's JavaScript Fundamentals class";
+  exports.createdBy = "Humbly hand-crafted by HK Kahng";
+  exports.createdOn = "Coded in November 2016";
+
+  exports.isBrowser = function() {
+    return (typeof window === "undefined" || window === null ? false : true);
+  };
+
+  exports.monthName = function(number) {
+    var names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return names[number - 1];
+  };
+
+  exports.assignGrade = function(score) {
+    if (score <= 60) {
+      return "F";
+    } else if (score < 70) {
+      return "D";
+    } else if (score < 80) {
+      return "C";
+    } else if (score < 90) {
+      return "B";
+    } else {
+      return "A";
+    }
+  };
+
+  exports.fizzBuzz = function(number) {
+    var returnString = "";
+    for (var i = 1; i <= number; i++) {
+      if (i % 15 === 0) {
+        returnString += "fizzbuzz | ";
+      } else if (i % 5 === 0) {
+        returnString += "buzz | ";
+      } else if (i % 3 === 0) {
+        returnString += "fizz | ";
+      } else {
+        returnString += i + " | ";
+      }
+    }
+    return returnString;
+  };
+
+  exports.isPrime = function(inputNumber) {
+    if (inputNumber <= 0) {
+      return false;
+    } else if (inputNumber === 2 ) {
+      return true;
+    } else {
+      for (var i = 2; i <= Math.sqrt(inputNumber)/2; i++) {
+        if (inputNumber % i === 0) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  };
+
+  exports.readingList = function(library) {
+    var outputString = "";
+    library.forEach(function(book) {
+      outputString += (book.readingStatus ? "I am currently reading " : "I have yet to read ");
+      outputString += book.title + " by " + book.author + '\n';
+    });
+    return outputString;
+  };
+
+  exports.Vehicle = function(type, line, lat, long, speed, full, vehicleNumber, late) {
+    this.type = type;
+    this.line = line;
+    this.lat = lat;
+    this.long = long;
+    this.speed = parseInt(speed);
+    this.full = full;
+    this.vehicleNumber = parseInt(vehicleNumber);
+    this.late = parseInt(late);
+  };
+
+  exports.Vehicle.prototype.displayInfo = function() {
+    var returnArray = [];
+    returnArray.push(this.type);
+    returnArray.push(this.line);
+    returnArray.push([this.lat, this.long]);
+    return returnArray;
+  };
+
+  exports.Vehicle.prototype.updateLatLong = function(newLat, newLong) {
+    this.lat = newLat;
+    this.long = newLong;
+  };
+
+  exports.Vector = function(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+
+  exports.Vector.prototype.plus = function(vector) {
+    this.x += vector.x;
+    this.y += vector.y;
+    return this;
+  };
+
+  exports.Vector.prototype.minus = function(vector) {
+    this.x -= vector.x;
+    this.y -= vector.y;
+    return this;
+  };
+
+})(typeof window === "undefined" || window === null ? global.classToolBox = {} : window.classToolBox = {});
+
+var library = [
+  {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+  },
+  {
+    author: 'Walter Isaacson',
+    title: 'Steve Jobs',
+    readingStatus: true
+  },
+  {
+    author: 'Suzanne Collins',
+    title:  'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+}];
+
+console.log(classToolBox.projectName);
+console.log(classToolBox.createdFor);
+console.log(classToolBox.createdBy);
+console.log(classToolBox.createdOn);
+console.log("Is this a browser? " + classToolBox.isBrowser());
+console.log(classToolBox.monthName(10));
+console.log("A score of 95 gets a grade of " + classToolBox.assignGrade(95));
+console.log(classToolBox.fizzBuzz(45));
+console.log("Is 201 a Prime Number? " + classToolBox.isPrime(201));
+console.log(classToolBox.readingList(library));
+
+var busLine12 = new classToolBox.Vehicle("Bus", "12", -122.2, 45.52, 30, false, 1520, 2);
+console.log(busLine12.displayInfo(busLine12.updateLatLong(-122.4, 45.56)));
+console.log(new classToolBox.Vector(1, 2).minus(new classToolBox.Vector(2, 3)));
+console.log(new classToolBox.Vector(1, 2).plus(new classToolBox.Vector(2, 3)));
--- tuesday.js
+++ tuesday.js
@@ -32,9 +32,6 @@ isBrowser() ? $("#outputAreaWeekDay").text(weekDay.name(weekDay.number("Saturday
 (function(exports) {
   var taxRate = 9;
   var inventory = [["Phone", 399.99], ["Accessory", 19.99]];
-  var runningBalance = 0;
-  var itemTotal = 0;
-  var output = "";
 
   exports.calcItemTotal = function(purchasePrice, taxRate) {
     return purchasePrice * (1 + (taxRate/100));
@@ -44,4 +41,32 @@ isBrowser() ? $("#outputAreaWeekDay").text(weekDay.name(weekDay.number("Saturday
     return "$" + amount.toFixed(2);
   };
 
+  exports.withinBudget = function(amount, availableFunds, spendingThreshold) {
+    return amount > availableFunds - spendingThreshold ? false : true;
+  };
+
+  exports.buyStuff = function(availableFunds, spendingThreshold) {
+    var runningBalance = 0;
+    var itemTotal = 0;
+    var output = "";
+
+    for (var i = 0; i < 2; i++) {
+      while (this.withinBudget(runningBalance, availableFunds, spendingThreshold)) {
+        itemTotal = this.calcItemTotal(inventory[i][1], taxRate);
+        if (!this.withinBudget(runningBalance + itemTotal, availableFunds, spendingThreshold)) {
+          break;
+        } else {
+          runningBalance += itemTotal;
+          output += (inventory[i][0] + ' purchased for ' + this.printDollar(itemTotal) + ' (Current balance: ' + this.printDollar(runningBalance) + ')\n');
+        }
+      }
+    }
+
+    return "Available Funds: " + this.printDollar(availableFunds) + "\n" + "Spending Threshold: " + this.printDollar(spendingThreshold) + "\n" + output;
+  }
+
 })(typeof window === 'undefined' || window === null ? global.phoneApp = {} : window.phoneApp = {});
+
+console.log(phoneApp.printDollar(phoneApp.calcItemTotal(100,9)));
+console.log(phoneApp.withinBudget(phoneApp.calcItemTotal(499.99, 9), 1000, 300));
+isBrowser() ? $("#phoneShopper").text(phoneApp.buyStuff(3000, 300)) : console.log(phoneApp.buyStuff(3000, 300));
